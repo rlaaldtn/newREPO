4b59824e098afcff2c6b009f2c6c9a953e1ccda9
AustinYeon <umovito@gmail.com>
2016-12-26 15:47:21 +0900

    하하하

--- demo/src/main/java/com/example/Home.java
+++ demo/src/main/java/com/example/Home.java
@@ -1,14 +1,12 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Home {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(greetingH.class, args);
+        SpringApplication.run(Home.class, args);
     }
 }
--- demo/src/main/java/com/example/greetingController.java
+++ demo/src/main/java/com/example/greetingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class greetingH {
+public class greetingController {
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
