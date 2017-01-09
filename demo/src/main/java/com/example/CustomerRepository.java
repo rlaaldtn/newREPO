ffd02344aaea594327d241b787488edb6c1e22fb
rlaaldtn <mskim0430@gmail.com>
2017-01-09 16:11:14 +0900

    제준찡의 코드를 받기 위해 다 갈아버림

--- demo/src/main/java/com/example/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-	public Customer findByCustomerId(String id);
-}
