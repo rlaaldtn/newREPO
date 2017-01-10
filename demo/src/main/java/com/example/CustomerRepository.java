41f271b0df7e7a9f4a45d9118e95d9e4b3987423
rlaaldtn <mskim0430@gmail.com>
2017-01-10 20:25:36 +0900

    커밋

--- demo/src/main/java/com/example/CustomerRepository.java
+++ demo/src/main/java/com/example/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 	public Customer findByCustomerId(String id);
 }
